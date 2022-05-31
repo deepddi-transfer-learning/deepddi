7bc5d10650e2e38ed3351f807f395bcb3cc2098a
Couson <couson@foxmail.com>
2022-05-31 01:05:25 -0700

    saved work

--- main.py
+++ main.py
@@ -11,6 +11,27 @@ OUTPUT_DIR = './output'
 OUTPUT_TXT = 'output/Final_annotated_DDI_result.txt'
 SIGNIFICANCE = 0.8
 DFI_INPUT_DRUGS = []
+DDI_OTHER_DRUGS = []
+
+food_comp = pd.read_csv('./database/food_compounds_lookup.csv')
+# pd.DataFrame({'Name': food.orig_food_common_name\
+#                           .str.replace('(', '').str.replace(')', '')\
+#                           .str.split()\
+#                           .str[0].unique()}
+#             ).to_csv('./database/food.csv', index = None)
+
+food_loc = food_comp.orig_food_common_name\
+            .str.replace('(', '')\
+            .str.replace(')', '')\
+            .str.split().str[0]
+comp_loc = food_comp.name.str.lower()
+
+food2comp = defaultdict(set)
+comp2food = defaultdict(set)
+for f,c in zip(food_loc, comp_loc):
+    food2comp[f].add(c)
+    comp2food[c].add(f)
+    
 
 def regex_search(desc, pools):
     # assume desc is lowercased
@@ -25,38 +46,56 @@ def ingest_input(input_json, interaction_type, input_fp = INPUT_PATH,
                  compounds_path = './database/Drug_info_combined.csv',
                  food_path = './database/food.csv'):
     assert interaction_type.lower() in ['ddi', 'dfi'], 'API not supported'
-    out = []
-    
+    first_line = []
+    second_line = []
     # handle ddi
     if interaction_type.lower() == 'ddi':
         drug_pools = pd.read_csv(compounds_path).Name
         
         cur_desc = input_json['current_drug']['drug_desc'].lower()
-        drug_name = input_json['current_drug']['drug_title']
-        out += [drug_name+'|'+i for i in regex_search(cur_desc, drug_pools)]
+        drug_title = input_json['current_drug']['drug_title']
+        
+        drug_search = regex_search(cur_desc, drug_pools)
+        assert not drug_search, ('Drug: %s not Found' % drug_title)
+            
+        first_line += [drug_title+'|'+i for i in drug_search]
         for drug in input_json['other_drug']:
-            each_name = drug['drug_title']
+            DDI_OTHER_DRUGS.append(drug['drug_title'])
             other_desc = drug['drug_desc'].lower()
-            found=[each_name+'|'+ i for i in regex_search(other_desc, drug_pools)]
-            out += found
+            other_search = regex_search(other_desc, drug_pools)
+            
+            if not other_search:
+                print('Drug: %s not Found' % drug['drug_title'])
+                continue
+            second_line += [drug['drug_title']+'|'+ i for i in other_search]
     # handle dfi
     else:
         drug_pools = pd.read_csv(compounds_path).Name
         food_pools = pd.read_csv(food_path).Name
-        cur_desc = input_json['current_drug']['drug_desc'].lower()
-        drug_name = input_json['current_drug']['drug_title']
-        out += [drug_name+'|'+i for i in regex_search(cur_desc, drug_pools)][:1]
-        # handle drug not found
-        assert len(regex_search(cur_desc, drug_pools))>0 , 'We can not found drug: '+drug_name
-        for food in input_json['food']:
-            out+= regex_search(food.lower(), food_pools)      
         
+        for drug in input_json['drug_list']:
+            DFI_INPUT_DRUGS.append(drug['drug_title'])
+            drug_search = regex_search(drug['drug_desc'], drug_pools)
+            
+            if not drug_search:
+                print('Drug: %s not Found' % drug['drug_title'])
+                continue
+            first_line += [drug['drug_title'] + '|' + i for i in drug_search]
+        
+        for food in input_json['food_list']:
+            food_search = food2comp[food] 
+            if not food_search:
+                print('Food: %s not Found' % food)
+                continue
+            second_line += [food + '|' + i for i in food_search]
+            
+    # TODO handle not-found case           
     if os.path.exists(input_fp):
         os.remove(input_fp)
 
     with open(input_fp, 'w') as fw:
-        to_write = '\n'.join(out) + '\n'
-        fw.write(to_write)
+        fw.write(','.join(first_line) + '\n')
+        fw.write(','.join(second_line) + '\n')
 
 
 def run(input_json,interaction_type,thres=SIGNIFICANCE):
@@ -73,10 +112,6 @@ def run(input_json,interaction_type,thres=SIGNIFICANCE):
     except AssertionError:
         return None
 
-def format_output():
-    # TODO: handle DFI comp2food 
-    # TODO: hanlde interaction id2desc
-    pass
 
 def collect_output(thres = SIGNIFICANCE, out_txt = OUTPUT_TXT):
     res = pd.read_csv(out_txt,
@@ -84,42 +119,48 @@ def collect_output(thres = SIGNIFICANCE, out_txt = OUTPUT_TXT):
                       header=0)[['drug1', 'drug2',
                                           'DDI_prob', 'DDI_prob_std',
                                           'Confidence_DDI', 'Sentence',
-                                          'Interaction_type']]
+                                          'Side effects (left)',
+                                          'Side effects (right)']]
     
     temp = res.loc[(res.Confidence_DDI == 1) &\
                    (res.DDI_prob >= thres)]
     
+    if DDI_OTHER_DRUGS:
+        temp = temp.loc[(res.drug1.str.contains('|'.join(DDI_OTHER_DRUGS))) |\
+                        (res.drug2.str.contains('|'.join(DDI_OTHER_DRUGS)))]
+        
     if DFI_INPUT_DRUGS:
         temp = temp.loc[(res.drug1.str.contains('|'.join(DFI_INPUT_DRUGS))) |\
                         (res.drug2.str.contains('|'.join(DFI_INPUT_DRUGS)))]
+    
     out = {}
-    out['interations'] = []
+    out['interactions'] = []
     for line in temp.iterrows():
         inner_out = {}
         row = line[1].values
     #     print(row)
-        drug_pair = [row[0], row[1]]
+        other_drug = row[1] ###
         interaction_desc = row[5]
         side_effect = {}
         side_effect['probability'] = row[2]
         side_effect['side_effect_id'] = row[6]
-        inner_out['drug_pair'] = drug_pair
+        inner_out['other_drug'] = other_drug
         inner_out['interaction_desc'] = interaction_desc
         inner_out['side_effect'] = side_effect
         out['interations'].append(inner_out)
     return out
 
 # Example of calling DFI API
-dfi_sample_input = {'current_drug': {'drug_title': 'Bad Drug', 'drug_desc': '?xxxxxx xxx Acetaminophen xxx xxx'},
- 'food': ['lemon', 'orange']}
+dfi_sample_input = {'drug_list': [{'drug_title': 'Drug_C', 'drug_desc': '? Vitamin A '}, {'drug_title': 'Drug_D', 'drug_desc': '? Vitamin C '}],
+ 'food_list': ['lemon', 'orange']}
 
 # Example of calling DDI API
 ddi_sample_input =  {'current_drug': {'drug_title': 'Good Drug', 'drug_desc': '! Biotin !'},
- 'other_drug': [{'drug_title': 'So-so Drug', 'drug_desc': ' cool  Vitamin C and vitamin A'},
-                {'drug_title': 'Pain-Killer', 'drug_desc': ' very good Aspirin Acetaminophen'}]}
+ 'other_drug': [{'drug_title': 'Drug_A', 'drug_desc': ' cool  Vitamin C '},
+                {'drug_title': 'Drug_B', 'drug_desc': ' very good Aspirin Acetaminophen'}]}
 
 # ALL you need to call is func 'run(input_json,type)'
-print(run(dfi_sample_input,'DFI'))
+# print(run(dfi_sample_input,'DFI'))
 # print(run(ddi_sample_input,'DDI'))
 
     
